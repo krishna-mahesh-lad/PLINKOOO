edcc6ddcf68e78d043c8d0071bf537b2be1c7e09
krishna-mahesh-lad <68733354+krishna-mahesh-lad@users.noreply.github.com>
2020-09-24 12:04:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+
+function setup() {
+  createCanvas(480,800);
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(240,780,1200,20);
+}
+
+function draw() {
+  background(255,255,255);  
+  Engine.update(engine);
+
+  ground.display();
+}
